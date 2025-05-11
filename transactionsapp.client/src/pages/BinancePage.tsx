e6e8a4f458077d7a20601501ef2287a56a833e1a
João Costa <138248786+jaozincosta@users.noreply.github.com>
2025-05-11 20:04:28 -0300

    Adicionando mediaquerys

--- transactionsapp.client/src/pages/BinancePage.tsx
+++ transactionsapp.client/src/pages/BinancePage.tsx
@@ -66,6 +66,18 @@ const Button = styled.button`
     }
 `;
 
+const size = {
+    mobile: '768px',
+    tablet: '1024px',
+};
+
+export const device = {
+    mobile: `(max-width: ${size.mobile})`,
+    tablet: `(min-width: ${size.mobile}) and (max-width: ${size.tablet})`,
+    desktop: `(min-width: ${size.tablet})`,
+};
+
+
 interface SymbolData {
     symbol: string;
 }
